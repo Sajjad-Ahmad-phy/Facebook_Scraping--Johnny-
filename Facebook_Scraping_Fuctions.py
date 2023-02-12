11275b49fdec6b9e412f4a2e65759480594702d7
Sajjad-Ahmad-Phy <sajjadkh508@gmail.com>
2023-02-12 18:21:08 +0500

    new

--- Facebook_Scraping_Fuctions.py
+++ Facebook_Scraping_Fuctions.py
@@ -2,103 +2,152 @@ import os
 from openpyxl import Workbook, load_workbook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pyautogui
 from time import sleep
 
-chromeOptions = webdriver.ChromeOptions()
-
+option = webdriver.ChromeOptions()
+option.add_argument('--disable-blink-features=AutomationControlled')
+option.add_argument('--ignore-certificate-errors')
+option.add_argument('--user-data-dir=C:\\Users\\lenovo\\AppData\\Local\\Google\\Chrome\\User Data\\Default')
+option.add_argument('--profile-directory=Person 1')
+option.add_argument('--disable-blink-features=AutomationControlled')
 driver = webdriver.Chrome()
-
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
-"Login"
-
-username = ""
-password = ""
-
 "Requesting Facebook"
 driver.get("https://www.facebook.com/")
 driver.maximize_window()
-sleep(1)
 
-driver.find_element('xpath', """ //*[@id="email"]""").send_keys(username)
-driver.find_element('xpath', """  //*[@id="pass"]""").send_keys(password)
-driver.find_element(By.NAME, """login""").click()
-sleep(10)
+def Login_Facebook(username, password):
+    driver.find_element('xpath', """ //*[@id="email"]""").send_keys(username)
+    driver.find_element('xpath', """  //*[@id="pass"]""").send_keys(password)
+    driver.find_element(By.NAME, """login""").click()
+    sleep(2)
 
-"opening the existing csv files"
-work_book = load_workbook(filename='URLs_List.xlsx')
 
-# TO make active the excel file
-ACT_Sheet = work_book.active
-
-"O/p Excel"
-W_B = Workbook()
-sheet_N = W_B.active
-sheet_N.title = 'Data sheet1'
-sheet_N.append(['Links', 'Title', 'price', 'Listed Data', 'description', 'Driven', 'Transmission', 'Exterior Color',
-                'Interior color', 'Conditions', 'Fuel Type', 'Engine size', 'Owners'])
-
-# W_B = load_workbook(filename='Vehicles Info1.xlsx')
-# sheet_N = W_B.active
-
-"No of iterations"
-No_of_Iterrations = len(ACT_Sheet['C'])
-
-"URL for loop should be start from here"
-for url in range(No_of_Iterrations):
-    row_no = url + 2
-    c = ACT_Sheet.cell(row=row_no, column=1)
-    URL = f"{c.value}"
-
-    headers = {
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
-    driver.get(URL)
-    sleep(1)
-    Final_List = []
-    Final_List.append(URL)
-
-    data = driver.find_element(By.XPATH, " /html/body ")
-    List = data.text.split('\n')
-    sleep(1)
-
-    if List[32] == 'This listing is no longer available':
-        continue
+"**************Data Scraping*****************************"
+def OutPut_Data(ACT_Sheet,sheet_N, Name_of_Output_Excel_file, W_B):
 
     try:
-        "Appending Title price and listed data"
-        Final_List.append(List[31])
-        Final_List.append(List[32])
-        Final_List.append(List[33])
+        Col = sheet_N['A']
+        Col_list = [Col[x].value for x in range(len(Col))]
+
+        for url in range(len(ACT_Sheet['A'])):
+            if url not in Col_list:
+                row_no = url+2
+                c = ACT_Sheet.cell(row=row_no, column=1)
+                URL = f"{c.value}"
+                driver.get(URL)
+                sleep(1)
+                Final_List = []
+                Final_List.append(URL)
+
+                data = driver.find_element(By.XPATH, " /html/body ")
+                List = data.text.split('\n')
+                sleep(1)
+
+                if List[32] == 'This listing is no longer available':
+                    continue
+
+                try:
+                    "Appending Title price and listed data"
+                    Final_List.append(List[31])
+                    Final_List.append(List[32])
+                    Final_List.append(List[33])
+                except:
+                    pass
+                try:
+                    D1 = List.index("Seller's description")
+                    D2 = List.index("Location is approximate")
+                    Des1 = List[D1+1:D2-1]
+                    "Seller's description"
+                    Description = " ".join([str(elem) for elem in Des1])
+                    Final_List.append(Description)
+                except:
+                    Final_List.append('This listing is no longer available')
+
+                try:
+                    "Engine details ..."
+                    a1 = List.index('About this vehicle')
+                    a2 = List.index("Seller's description")
+                    about = List[a1:a2 - 1]
+                    About_the_Vehicle = " ".join([str(ele) for ele in about])
+                    Final_List.append(About_the_Vehicle)
+
+                except:
+                    pass
+                try:
+                    # links = driver.find_elements(By.TAG_NAME, 'img')
+                    links = driver.find_elements(By.TAG_NAME, 'img')
+                    for link in links:
+                        href = link.get_attribute('src')
+                        if href is not None:
+                            Final_List.append(href)
+                except:
+                    pass
+
+                sheet_N.append(Final_List)
+                clear_screen()
+                print(url)
+
+                "Save o/p Excel"
+                W_B.save(f"D:\\PYTHON PROJECTS\\Facebook_Scraping--Johnny-\\Excel files\\{Name_of_Output_Excel_file}.xlsx")
+
     except:
         pass
-    try:
-        D1 = List.index("Seller's description")
-        D2 = List.index("Location is approximate")
-        Des1 = List[D1 + 1:D2 - 1]
-        "Seller's description"
-        Description = " ".join([str(elem) for elem in Des1])
-        Final_List.append(Description)
-    except:
-        Final_List.append('This listing is no longer available')
-    try:
-        "Engine details ..."
-        a1 = List.index('About this vehicle')
-        a2 = List.index("Seller's description")
-        about = List[a1:a2 - 1]
-        About_the_Vehicle = " ".join([str(ele) for ele in about])
-        Final_List.append(About_the_Vehicle)
 
-    except:
-        pass
 
-    sheet_N.append(Final_List)
-    clear_screen()
-    print(url)
+"**************Links scraping**********************"
 
-    "Save o/p Excel"
-    W_B.save('Vehicles Info.xlsx')
+class Links_Scraping_Functions:
 
-driver.quit()
+    def __init__(self, CarModel_Url):
+        self.Carmodel_Url = CarModel_Url
+
+    def Output_Link(self):
+        driver.get(self.Carmodel_Url)
+        try:
+            # while True:
+            #     driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            while True:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+                sleep(2)
+                # calculate the new scroll height and compare it with the previous scroll height
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    break
+                last_height = new_height
+        except:
+            pass
+
+
+        self.List_of_links = []
+        self.links = driver.find_elements(By.TAG_NAME, 'a')
+        for link in self.links:
+            href = link.get_attribute('href')
+            if href is not None:
+                 if f'{href[0:42]}' == 'https://www.facebook.com/marketplace/item/':
+                    self.List_of_links.append(href)
+
+    def Old_output_Exfile(self, Excel_file_name):
+        self.Excel_file_name = Excel_file_name
+        Old_WB = load_workbook(filename=f'D:\\PYTHON PROJECTS\\Facebook_Scraping--Johnny-\\Excel files\\{self.Excel_file_name}.xlsx')
+        Old_Sheet = Old_WB.active
+        for link in self.List_of_links:
+            Old_Sheet.append([f'{link}'])
+
+        Old_WB.save(f'D:\\PYTHON PROJECTS\\Facebook_Scraping--Johnny-\\Excel files\\{self.Excel_file_name}.xlsx')
+
+    def New_output_Exfile(self, New_Excel_file_name):
+        self.New_Excel_file_name = New_Excel_file_name
+        New_WB = Workbook()
+        New_Sheet = New_WB.active
+
+        for link in self.List_of_links:
+            New_Sheet.append([f'{link}'])
+
+        New_WB.save(f'D:\\PYTHON PROJECTS\\Facebook_Scraping--Johnny-\\Excel files\\{self.New_Excel_file_name}.xlsx')
--- Facebook_Scraping_main.py
+++ Facebook_Scraping_main.py
@@ -1,1 +1,106 @@
+"********************Links scraping portion***********************"
+
+CarsModel_Links_file_name = 'FBCarModelUrl'
+Cars_link_no = 0             #Cars' model link no
+
+#Enter WorkBook For Saving Links
+Links_Old_file = 'Excel file for all Cars links'
+Links_New_file = 'Excel file for all Cars links'
+
+
+"Login"
+username = "03078440508"
+password = "112223"
+
+def Links_scraping():
+
+    O_or_N = int(input('To Which File Do you want to scrape ****links*** of Cars. \n\t1. Old Excel file \n\t2. New Excel file \n\tEnter 1 for Old and 2 for New. \t'))
+
+    from Facebook_Scraping_Fuctions import load_workbook, Links_Scraping_Functions, Login_Facebook
+    "Login Facebook"
+    Login_Facebook(username, password)
+
+    cars_links_Wb = load_workbook(filename=f'D:\\PYTHON PROJECTS\\Facebook_Scraping--Johnny-\\Excel files\\{CarsModel_Links_file_name}.xlsx')
+    Cars_links_sheet = cars_links_Wb.active
+    Cars_links_iteration = len(Cars_links_sheet['B']) - Cars_link_no
+
+    if O_or_N == 1:     #old
+        try:
+            for li in range(Cars_links_iteration):
+                CarModel_Url = Cars_links_sheet.cell(row=li+1+Cars_link_no, column=2).value
+                Class = Links_Scraping_Functions(CarModel_Url)
+                Class.Output_Link()
+                Class.Old_output_Exfile(Links_Old_file)
+                print('Cars Model Link no: ', li+Cars_link_no+1)
+        except:
+            pass
+
+    elif O_or_N == 2:  #New
+        try:
+            for Li in range(Cars_links_iteration):
+                CarModel_url = Cars_links_sheet.cell(row=Li+1+Cars_link_no, column=2).value
+                Class = Links_Scraping_Functions(CarModel_url)
+                Class.Output_Link()
+                if Li < 1:
+                    Class.New_output_Exfile(Links_New_file)
+                elif Li >= 1:
+                    Class.Old_output_Exfile(Links_New_file)
+                print("Cars Model Link no: ", Li+1)
+        except:
+            pass
+    else:
+        print('Wrong Entry')
+
+
+"***********************************************************************************"
+"Cars' Data Scraping portion"
+Data_Old_file = "All cars details"         #output  cars' old data file
+Data_New_file = "All cars details"    #output car's New data file
+
+Links_file_name = 'Excel file for all Cars links'         #File's Name where All the links to all type of cars are stored
+
+def Data_Scraping():
+    O_or_N = int(input('To Which File Do you want to scrape ***Data*** of Cars. \n\t1. Old File\n\t2. New file \n\tEnter 1 for Old and 2 for New. \t\t'))
+
+    "login"
+    from Facebook_Scraping_Fuctions import load_workbook, Workbook, Login_Facebook
+    Login_Facebook(username, password)
+
+    Links_wb = load_workbook(f'D:\\PYTHON PROJECTS\\Facebook_Scraping--Johnny-\\Excel files\\{Links_file_name}.xlsx')
+    ACT_Sheet = Links_wb.active
+
+    from Facebook_Scraping_Fuctions import OutPut_Data
+
+    if O_or_N == 1:
+        W_B_O = load_workbook(filename=f'{Data_Old_file}.xlsx')
+        sheet_O = W_B_O.active
+        OutPut_Data(ACT_Sheet, sheet_O, Data_Old_file, W_B_O)
+
+    elif O_or_N == 2:
+        W_B_N = Workbook()
+        sheet_N = W_B_N.active
+        sheet_N.append(
+            ['Links', 'Title', 'price', 'Listed Data', 'description', 'Driven', 'Transmission', 'Exterior Color',
+             'Interior color', 'Conditions', 'Fuel Type', 'Engine size', 'Owners', 'Car links'])
+        OutPut_Data(ACT_Sheet, sheet_N, Data_New_file, W_B_N)
+
+    else:
+        print('Wrong Entry')
+
+"Everything Starts from here"
+
+def main():
+    Decide = int(input('Enter: \n\t 1). Links Scraping\n\t 2). Data Scraping \n\t\t'))
+    if Decide == 1:
+        Links_scraping()
+
+    elif Decide == 2:
+        Data_Scraping()
+
+    else:
+        print("Wrong Entry for Making Decision")
+    print('Done')
+
+if __name__ == '__main__':
+    main()
 
